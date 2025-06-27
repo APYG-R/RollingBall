d6c69b292d213d567a6f1d11925b894ef4bdedd5
APYG-R <attapinya.r@ku.th>
2025-06-27 15:40:50 +0700

    Add files via upload

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -3,10 +3,28 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class RollingBall extends JPanel{
+public class RollingBall extends JPanel implements ActionListener{
+        int x = 100 , y = 90;
+        int dx = 4 , dy = 4;
+        int start_angle = 0;
+        int size = 80;
+
+    public RollingBall(){
+        Timer timer1 = new Timer(20, this);
+        timer1.start();
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(100, 90, 80, 80);
-        g.fillArc(100, 90, 80, 80, 0, 180);
+        g.drawOval(x , y, size, size);
+        g.fillArc(x , y, size, size, start_angle, 180);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        x-=2;
+        start_angle +=5;
+        if (x <= -size) x = getWidth();
+        if(start_angle>=360) start_angle = 0;
+        repaint();
     }
 }
